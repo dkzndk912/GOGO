395a43473653dc1043b5dcc967ec99e1870ad91e
dkzndk912 <dkzndk912@gmail.com>
2023-09-25 14:59:29 +0900

    week05_v0.5

--- week05/main.go
+++ week05/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("iNPUT SCORE : ")
 	reader := bufio.NewReader(os.Stdin)
-	InputScore, _ := reader.ReadString('\n') // Option 1, ignore the errot return value with the blank identifier
+	InputScore, err := reader.ReadString('\n') // Option 2
+	log.Fatal(err)
 	fmt.Println(InputScore)
 }
