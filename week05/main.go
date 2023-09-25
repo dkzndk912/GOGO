34d9504b1ad2957ed71f97f8cda14fcd91e8bbb8
dkzndk912 <dkzndk912@gmail.com>
2023-09-25 15:19:54 +0900

    week05_v0.8

--- week05/main.go
+++ week05/main.go
@@ -5,15 +5,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("iNPUT SCORE : ")
 	reader := bufio.NewReader(os.Stdin)
-	InputScore, err := reader.ReadString('\n') // Option 2
+	InputScoreString, err := reader.ReadString('\n') // Option 2
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	InputScoreString = strings.TrimSpace(InputScoreString)      // remove space
+	InputScore, err := strconv.ParseFloat(InputScoreString, 32) // casting
 
 	if InputScore >= 90 {
 		grade := "A grade!"
