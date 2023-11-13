0bc747c102464552c67974ca611d0441ae96d7df
dkzndk912 <dkzndk912@gmail.com>
2023-11-13 14:24:15 +0900

    v3.2

--- week12/main.go
+++ week12/main.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var s []int
+	// var s []int
 	// s = make([]int, 5)
 
 	// s := make([]int, 5)
 
 	s := []int{0, 0, 0, 0, 0}
+
+	s[4] = 99
+	s[2] = 91
 
 	for _, v := range s {
 		fmt.Println(v)
