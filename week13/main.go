92fe1250cca0429946f9d75ccae72cfe482c49ce
dkzndk912 <dkzndk912@gmail.com>
2023-11-20 14:10:13 +0900

    v13w.0

--- week13/main.go
+++ week13/main.go
@@ -6,7 +6,10 @@ import (
 
 func main() {
 	var a []string
+	var b []bool
 	a = make([]string, 4, 5)
-	fmt.Printf("%#v\n", a)
+	b = make([]bool, 4, 5)
+	fmt.Printf("%#v %#v\n", a, b)
 	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
 }
